65b6424ba43c233ac80ddcb8978e9014aa1e6f4e
SoftJoelWire <Joel.Robinson@softwire.com>
2024-07-01 16:56:29 +0100

    Part 2 complete

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,35 @@
 // This is our main function
 function fizzbuzz() {
     // Put your code here...
-    for(let i=1; i<=100; i++) {
-        //start with fizzbuzz case
-        if(i%5 == 0 && i%3 == 0) {console.log("Fizzbuzz");}
+    for(let i=1; i<=300; i++) {
+        //init message
+        let message = [];
 
-        //check fizz/buzz cases
-        else if(i%3 == 0) {console.log("Fizz");}
-        else if(i%5 == 0) {console.log("Buzz");}
 
-        //default case
-        else {console.log(i);}
+        //check fizz/fezz/buzz/bang cases
+        if(i%3 == 0) {message.push("Fizz");}
+        if(i%13 == 0) {message.push("Fezz");}
+        if(i%5 == 0) {message.push("Buzz");}
+        if(i%7 == 0) {message.push("Bang");}
+
+        //check bong case
+        if(i%11 == 0) {
+            message = ["Bong"];
+            //this case is unused for 1<=i<=100
+            if(i%13 == 0) {message.unshift("Fezz");}
+        }
+        
+        //check reverse case
+        if(i%17 == 0) {
+            message.reverse();
+        }
+
+        //check for number case or merge array
+        if(message.length == 0) {message = i;} else {message = message.join("");}
+
+        //print message
+        console.log(message);
     }
-
 }
 
 // Now, we run the main function:
