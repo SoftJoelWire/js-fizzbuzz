eac988704f2e423ad56f7df8743592931d85cc85
SoftJoelWire <Joel.Robinson@softwire.com>
2024-07-02 09:58:53 +0100

    added argument sanity check

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,15 @@
-const { log } = require("console");
+const { log, assert } = require("console");
 const process = require("process");
 
+class ConArgsError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "ConArgsError";
+    }
+}
+
 // This is our main function
-function fizzbuzz(maxRange, conArgs) {
+function fizzbuzz(maxRange, conArgsMap) {
     // Put your code here...
     for(let i=1; i<=maxRange; i++) {
         //init message
@@ -10,20 +17,20 @@ function fizzbuzz(maxRange, conArgs) {
 
 
         //check fizz/fezz/buzz/bang cases
-        if(i%3 == 0 && conArgs.get('3')) {message.push("Fizz");}
-        if(i%13 == 0 && conArgs.get('13')) {message.push("Fezz");}
-        if(i%5 == 0 && conArgs.get('5')) {message.push("Buzz");}
-        if(i%7 == 0 && conArgs.get('7')) {message.push("Bang");}
+        if(i%3 == 0 && conArgsMap.get('3')) {message.push("Fizz");}
+        if(i%13 == 0 && conArgsMap.get('13')) {message.push("Fezz");}
+        if(i%5 == 0 && conArgsMap.get('5')) {message.push("Buzz");}
+        if(i%7 == 0 && conArgsMap.get('7')) {message.push("Bang");}
 
         //check bong case
-        if(i%11 == 0 && conArgs.get('11')) {
+        if(i%11 == 0 && conArgsMap.get('11')) {
             message = ["Bong"];
             //this case is unused for 1<=i<=100
-            if(i%13 == 0 && conArgs.get('13')) {message.unshift("Fezz");}
+            if(i%13 == 0 && conArgsMap.get('13')) {message.unshift("Fezz");}
         }
         
         //check reverse case
-        if(i%17 == 0 && conArgs.get('17')) {
+        if(i%17 == 0 && conArgsMap.get('17')) {
             message.reverse();
         }
 
@@ -39,7 +46,8 @@ function fizzbuzz(maxRange, conArgs) {
 function main() {
 
     var maxRange = 100
-    var conArgs = new Map([["3", true], ["5", true], ["7", true], ["11", true], ["13", true], ["17", true]]);
+    var conArgs = ["3", "5", "7", "11", "13", "17"]
+    var conArgsMap = new Map([["3", true], ["5", true], ["7", true], ["11", true], ["13", true], ["17", true]]);
 
 
     //check for user prompt
@@ -55,14 +63,22 @@ function main() {
             maxArgs = Math.min(9, argvLen);
 
             //init condition args map data struct
-            for(let i=3; i<argvLen; i++) {
-                conArgs.set(process.argv[i], false);
+            try {
+                for(let i=3; i<argvLen; i++) {
+                    conditionNumStr = process.argv[i] 
+                    console.log(conArgs.includes(conditionNumStr));
+                    conArgs.includes(conditionNumStr) ? conArgsMap.set(process.argv[i], false) : (() => {throw new ConArgsError(`invalid condition argument: ${conditionNumStr}`);})();
+                }
+            } catch (e) {
+                console.error(e.name);
+                console.error(e.message);
+                return;
             }
         }
     }
 
     // Now, we run the main function:
-    fizzbuzz(maxRange=maxRange, conArgs=conArgs);
+    fizzbuzz(maxRange=maxRange, conArgsMap=conArgsMap);
 }
 
 main();
