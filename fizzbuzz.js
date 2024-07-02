42937b1818bfd293e71d4aa95c710c5192a40360
SoftJoelWire <Joel.Robinson@softwire.com>
2024-07-02 09:34:58 +0100

    stretch goal 2, conditional arguments, implemented

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,7 @@ const { log } = require("console");
 const process = require("process");
 
 // This is our main function
-function fizzbuzz(maxRange) {
+function fizzbuzz(maxRange, conArgs) {
     // Put your code here...
     for(let i=1; i<=maxRange; i++) {
         //init message
@@ -10,20 +10,20 @@ function fizzbuzz(maxRange) {
 
 
         //check fizz/fezz/buzz/bang cases
-        if(i%3 == 0) {message.push("Fizz");}
-        if(i%13 == 0) {message.push("Fezz");}
-        if(i%5 == 0) {message.push("Buzz");}
-        if(i%7 == 0) {message.push("Bang");}
+        if(i%3 == 0 && conArgs.get('3')) {message.push("Fizz");}
+        if(i%13 == 0 && conArgs.get('13')) {message.push("Fezz");}
+        if(i%5 == 0 && conArgs.get('5')) {message.push("Buzz");}
+        if(i%7 == 0 && conArgs.get('7')) {message.push("Bang");}
 
         //check bong case
-        if(i%11 == 0) {
+        if(i%11 == 0 && conArgs.get('11')) {
             message = ["Bong"];
             //this case is unused for 1<=i<=100
-            if(i%13 == 0) {message.unshift("Fezz");}
+            if(i%13 == 0 && conArgs.get('13')) {message.unshift("Fezz");}
         }
         
         //check reverse case
-        if(i%17 == 0) {
+        if(i%17 == 0 && conArgs.get('17')) {
             message.reverse();
         }
 
@@ -39,10 +39,11 @@ function fizzbuzz(maxRange) {
 function main() {
 
     var maxRange = 100
+    var conArgs = new Map([["3", true], ["5", true], ["7", true], ["11", true], ["13", true], ["17", true]]);
 
 
     //check for user prompt
-    var argvLen = rocess.argv.length;
+    var argvLen = process.argv.length;
     if(argvLen > 2) {
         maxRange = process.argv[2];
 
@@ -50,14 +51,18 @@ function main() {
         //rules are true by default
         //entering in specific rule argument will disable rule
         if(argvLen > 3) {
+            //ensure only valid amounts of arguments are passed
+            maxArgs = Math.min(9, argvLen);
+
+            //init condition args map data struct
             for(let i=3; i<argvLen; i++) {
-                
+                conArgs.set(process.argv[i], false);
             }
         }
     }
 
     // Now, we run the main function:
-    fizzbuzz(maxRange=maxRange);
+    fizzbuzz(maxRange=maxRange, conArgs=conArgs);
 }
 
 main();
